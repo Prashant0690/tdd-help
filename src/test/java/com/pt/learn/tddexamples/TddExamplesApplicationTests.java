3957a41d1136153acc3e0f2917328bc4e6a750fa
Prashant Tiwari <prshnttiwari16@gmail.com>
2020-09-12 11:27:53 -0700

    first commit

---
+++ src/test/java/com/pt/learn/tddexamples/TddExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pt.learn.tddexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
